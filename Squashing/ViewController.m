bcc113f0828b304f7fd7c61b47ff77554a641af6
rajasekhar.uppari@netzen-business-solutions.com <raja-u>
2016-01-27 12:47:36 +0530

    Initial Commit

---
+++ Squashing/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Squashing
+//
+//  Created by Raja Sekhar on 27/01/16.
+//  Copyright © 2016 Mobi Esprits Software Pvt. Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
