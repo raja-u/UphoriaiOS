703433d33d7273caa20c95ddc1225e5f0081880f
srinivas-b <srinivas.bonkuri@netzen-business-solutions.com>
2016-01-27 18:15:49 +0530

    testing1

--- Squashing/ViewController.m
+++ Squashing/ViewController.m
@@ -18,6 +18,15 @@
     [super viewDidLoad];
     NSString *string = @"sekhar";
     NSLog(@"string:%@",string);
+<<<<<<< HEAD
+=======
+    //[super viewDidLoad];
+    NSString *string1 = @"sekhar";
+    NSLog(@"string:%@",string1);
+    NSString *string2 = @"sekhar";
+    NSLog(@"string:%@",string2);
+
+>>>>>>> ffe3573... Latest commit
     // Do any additional setup after loading the view, typically from a nib.
 }
 
