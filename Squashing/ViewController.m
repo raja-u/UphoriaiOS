f7346b2bbf7e41472fc59d1190beb27d911db1d1
srinivas-b <srinivas.bonkuri@netzen-business-solutions.com>
2016-01-29 13:07:34 +0530

    ficomit

--- Squashing/ViewController.m
+++ Squashing/ViewController.m
@@ -18,8 +18,8 @@
     [super viewDidLoad];
     NSString *string = @"sekhar";
     NSLog(@"string:%@",string);
-	NSString *string2 = @"sekhar";
-    NSLog(@"string:%@",string2);
+	NSString *string4 = @"sekhar";
+    NSLog(@"string:%@",string4);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
